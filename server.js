c66fe2aa99c7a8b7120ca1d953af670396a1c5e0
Pallab Karmakar <ookarmakarpallab1212@gmail.com>
2026-07-24 16:50:51 +0530

    fix ejs and port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5992;
 
 // Directories setup
 const mediaDir = path.join(__dirname, 'media');
